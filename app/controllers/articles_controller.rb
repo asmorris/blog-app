856a43c59781f8497b3b1e02e4bad505ab466590
Andrew Morrison <morro141@gmail.com>
2016-10-24 02:00:10 -0400

    Add edit, update actions and edit page

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,8 +18,22 @@ class ArticlesController < ApplicationController
   	end
   end
 
+  def edit
+  	@article = Article.find(params[:id])
+  end
+
   def show
   	@article = Article.find(params[:id])
+  end
+
+  def update
+  	@article = Article.find(params[:id])
+  	if @article.update(article_params)
+  		flash[:success] = 'Article was successfully updated'
+  		redirect_to article_path(@article)
+  	else
+  		render 'edit'
+  	end
   end
 
   private
