7fc702f7b13204bc0a22f1d3fcf28eb84cc295ff
Andrew Morrison <morro141@gmail.com>
2016-10-24 01:21:54 -0400

    Updat readme, add show, flash, error messaging

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,10 +11,15 @@ class ArticlesController < ApplicationController
   def create
   	@article = Article.new(article_params)
   	if @article.save
-  		redirect_to articles_show(@article)
+  		flash[:success] = 'Article was successfully created!'
+  		redirect_to article_path(@article)
   	else
   		render 'new'
   	end
+  end
+
+  def show
+  	@article = Article.find(params[:id])
   end
 
   private
