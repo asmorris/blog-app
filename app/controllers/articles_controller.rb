15806754fe6e986fc2c27ab69256675fe8f9ada2
Andrew Morrison <morro141@gmail.com>
2016-10-25 00:24:07 -0400

    Add user_id to article, with associations and validations. Add debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,7 +2,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:edit, :update, :show, :destroy]
 
 	def index
-		@articles = Article.all
+		@articles = Article.order('created_at DESC').all
 	end
 
   def new
@@ -11,6 +11,8 @@ class ArticlesController < ApplicationController
 
   def create
   	@article = Article.new(article_params)
+    # DO NOT LEAVE THIS
+    @article.user = User.first
   	if @article.save
   		flash[:success] = 'Article was successfully created!'
   		redirect_to article_path(@article)
