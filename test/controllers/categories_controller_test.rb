5962c216fe729179872f6d6000ff2a3678834e9f
Andrew Morrison <morro141@gmail.com>
2016-10-27 00:43:24 -0400

    Fix tests and update nav bar with categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -1,24 +1,34 @@
 require 'test_helper'
 
-class CategoriesControllerTest < ActionDispatch::IntegrationTest
+class CategoriesControllerTest < ActionController::TestCase
 
 	def setup
 		@category = Category.create(name: 'sports')
+		@user = User.create(username: 'Andrew', email: 'andrew@gmail.com',
+										 password: 'password', admin: true)
 	end
 
 	test 'should get index' do
-		get categories_path
+		get :index
 		assert_response :success
 	end
 
 	test 'should get new' do
-		get new_category_path
+		session[:user_id] = @user.id
+		get :new
 		assert_response :success
 	end
 
 	test 'should get show' do
-		get category_path(@category)
+		get(:show, {'id' => @category.id})
 		assert_response :success
+	end
+
+	test 'should redirect create when admin not logged in' do
+		assert_no_difference 'Category.count' do
+			post :create, category: { name: 'sports' }
+		end
+		assert_redirected_to categories_path
 	end
 
 end
